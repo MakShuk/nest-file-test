8c36e4b713578d5ba8e24b358465b2ac80eb8ff4
Maksim Shuklun <makshuk@bk.ru>
2023-11-11 13:42:34 +0300

    port update

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = 3002;
-  const app = await NestFactory.create(AppModule);
-  await app.listen(port);
-  console.log(`File Test Port: ${port}`);
+	const port = 3003;
+	const app = await NestFactory.create(AppModule);
+	await app.listen(port);
+	console.log(`File Test Port:${port}`);
 }
 bootstrap();
