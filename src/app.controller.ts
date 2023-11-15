f867eb0d1c8ab2135aaf70e21794c08c79befb04
Maksim Shuklun <makshuk@bk.ru>
2023-11-15 08:25:56 +0300

    add glob

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,10 @@ import {
 	Controller,
 	Get,
 	HttpStatus,
-	Param,
 	ParseFilePipe,
 	ParseFilePipeBuilder,
 	Post,
+	Query,
 	Res,
 	StreamableFile,
 	UploadedFile,
@@ -20,10 +20,9 @@ import { FileSizeValidationPipe } from './pipe/file-size-validation';
 import { MaxFileSize } from './pipe/validation';
 import { diskStorage } from 'multer';
 import { Request, Response } from 'express';
-import { createReadStream } from 'fs';
 import { join } from 'path';
 import * as fs from 'fs';
-import { downloadFile } from './lib/download-file';
+import { AppService } from './app.service';
 
 const UPLOAD_DIR = './upload/files/';
 const MAX_UPLOAD_SIZE = 10; // in MB
@@ -43,7 +42,7 @@ const defaultConfig = diskStorage({
 
 @Controller()
 export class AppController {
-	constructor() {}
+	constructor(private readonly appService: AppService) {}
 
 	@Post('upload-image')
 	@UseInterceptors(FileInterceptor('file'))
@@ -128,27 +127,39 @@ export class AppController {
 		console.log(files);
 	}
 
-	@Get('download/:id')
-	@Get()
-	getFile(@Res({ passthrough: true }) res: Response, @Param('id') id: string) {
-		const path = `/upload/files/${id}.jpg`;
-
+	@Get('img')
+	async getFile(
+		@Res({ passthrough: true }) res: Response,
+		@Query() queryParams: { folder: string; id: string },
+	) {
 		res.set({
 			'Content-Type': 'image/jpeg',
 			'Content-Disposition': 'attachment; filename="1.jpg"',
 		});
+
+		const path = `/resources/${queryParams.folder}/${queryParams.id}.jpg`;
 		const filePath = join(process.cwd(), path);
+		await this.appService.getFile();
 
-		if (!fs.existsSync(filePath)) {
-			return res.status(404).send('File not found');
+		try {
+			const stats = await new Promise((resolve, reject) => {
+				fs.stat(filePath, (err, stats) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(stats);
+					}
+				});
+			});
+
+			if (!stats) {
+				return res.status(404).send('File not found');
+			}
+
+			const file = fs.createReadStream(join(process.cwd(), path));
+			return new StreamableFile(file);
+		} catch (err) {
+			return res.status(404).send('File reading error');
 		}
-		const file = createReadStream(join(process.cwd(), path));
-		return new StreamableFile(file);
-	}
-
-	@Get('download-file')
-	async downloadFileToUrl(@Body() data: { url: string }) {
-		await downloadFile(data.url);
-		return 'OK';
 	}
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { glob } from 'glob';
 
 @Injectable()
 export class AppService {
 	getHello(): string {
 		return 'Hello World!';
+	}
+
+	async getFile() {
+		const jsfiles = await glob('**/*.ts', { ignore: 'node_modules/**' });
+		console.log(jsfiles);
 	}
 }
